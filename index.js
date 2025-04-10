8abc6903c4f41c47965de706c50d0c7532e239d6
Jehanzeb <jehanzebsiddiqui287@gmail.com>
2025-04-10 08:02:26 -0700

    ss

--- index.js
+++ index.js
@@ -6,37 +6,32 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log("User connected:", socket.id);
 
-  socket.on("join-room", (data) => {
-    const { roomId, email } = data;
-    console.log(`User with email ${email} joined room ${roomId}`);
+  socket.on("join-room", ({ roomId }) => {
     socket.join(roomId);
-    socket.emit("joined-room", { roomId });
-    socket.broadcast.to(roomId).emit("user-joined", { email });
+    socket.broadcast.to(roomId).emit("user-joined");
+    console.log(`User joined room: ${roomId}`);
   });
 
-  socket.on("offer", (data) => {
-    const { roomId, offer } = data;
+  socket.on("offer", ({ roomId, offer }) => {
     socket.broadcast.to(roomId).emit("offer", { offer });
   });
 
-  socket.on("answer", (data) => {
-    const { roomId, answer } = data;
+  socket.on("answer", ({ roomId, answer }) => {
     socket.broadcast.to(roomId).emit("answer", { answer });
   });
 
-  socket.on("ice-candidate", (data) => {
-    const { roomId, candidate } = data;
+  socket.on("ice-candidate", ({ roomId, candidate }) => {
     socket.broadcast.to(roomId).emit("ice-candidate", { candidate });
   });
 
@@ -46,9 +41,9 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Signaling server is up and running");
 });
 
 server.listen(8500, () => {
-  console.log("Server is running on port 8500");
+  console.log("Server listening on port 8500");
 });
