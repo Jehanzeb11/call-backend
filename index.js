affb1a3bff54349b5ff4bf36c5928829503e49b9
Jehanzeb <jehanzebsiddiqui287@gmail.com>
2025-04-11 05:47:22 -0700

    sd

--- index.js
+++ index.js
@@ -7,51 +7,58 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
+
+const rooms = {};
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   socket.on("join-room", ({ roomId }) => {
     socket.join(roomId);
-    const room = io.sockets.adapter.rooms.get(roomId);
-    const numClients = room ? room.size : 0;
 
-    if (numClients === 1) {
-      socket.emit("created");
-    } else {
-      socket.emit("joined");
-      socket.to(roomId).emit("ready");
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
     }
+
+    rooms[roomId].push(socket.id);
+    const numUsers = rooms[roomId].length;
+
+    if (numUsers === 1) {
+      socket.emit("created");
+    } else if (numUsers === 2) {
+      io.to(roomId).emit("ready");
+    } else {
+      socket.emit("room-full");
+    }
+
+    socket.on("disconnect", () => {
+      rooms[roomId] = rooms[roomId]?.filter((id) => id !== socket.id);
+      if (rooms[roomId]?.length === 0) delete rooms[roomId];
+    });
   });
 
   socket.on("offer", ({ roomId, offer }) => {
-    socket.to(roomId).emit("offer", { offer });
+    socket.broadcast.to(roomId).emit("offer", { offer });
   });
 
   socket.on("answer", ({ roomId, answer }) => {
-    socket.to(roomId).emit("answer", { answer });
+    socket.broadcast.to(roomId).emit("answer", { answer });
   });
 
   socket.on("ice-candidate", ({ roomId, candidate }) => {
-    socket.to(roomId).emit("ice-candidate", { candidate });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    socket.broadcast.to(roomId).emit("ice-candidate", { candidate });
   });
 });
 
 server.listen(8500, () => {
-  console.log("Server is running on http://localhost:8500");
+  console.log("Signaling server running on http://localhost:8500");
 });
-
 
 
 // const express = require("express");
