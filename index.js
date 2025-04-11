1aa92bf5559e3a325431b139fccf25db833d7839
Jehanzeb <jehanzebsiddiqui287@gmail.com>
2025-04-11 04:59:37 -0700

    dd

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ io.on("connection", (socket) => {
     console.log(`User ${email || socket.id} joining room ${roomId}`);
     socket.join(roomId);
 
+    socket.emit("joined-room", { roomId }); // ✅ <-- ADD THIS LINE
+
     // Initialize room if it doesn't exist
     if (!rooms[roomId]) rooms[roomId] = [];
     rooms[roomId].push(socket.id);
@@ -73,3 +75,59 @@ app.get("/", (req, res) => {
 server.listen(8500, () => {
   console.log("Signaling server listening on http://localhost:8500");
 });
+
+
+
+
+
+// const express = require("express");
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const cors = require("cors");
+
+// const app = express();
+// app.use(cors());
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("User connected:", socket.id);
+
+//   socket.on("join-room", (data) => {
+//     const { roomId, email } = data;
+//     console.log(`User with email ${email} joined room ${roomId}`);
+//     socket.join(roomId);
+//     socket.emit("joined-room", { roomId });
+//     socket.broadcast.to(roomId).emit("user-joined");
+//   });
+
+//   socket.on("offer", ({ roomId, offer }) => {
+//     socket.broadcast.to(roomId).emit("offer", { offer });
+//   });
+
+//   socket.on("answer", ({ roomId, answer }) => {
+//     socket.broadcast.to(roomId).emit("answer", { answer });
+//   });
+
+//   socket.on("ice-candidate", ({ roomId, candidate }) => {
+//     socket.broadcast.to(roomId).emit("ice-candidate", { candidate });
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected");
+//   });
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Signaling server is up and running");
+// });
+
+// server.listen(8500, () => {
+//   console.log("Server listening on port 8500");
+// });
